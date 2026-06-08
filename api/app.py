b2825c6dd5bfb9f425b40afecc3f6b0d52a3ea9e
K Vishnukanth Reddy <kallamadivishnu@gmail.com>
2026-06-08 10:35:19 +0530

    Added file based policy search

--- api/app.py
+++ api/app.py
@@ -1,27 +1,19 @@
 from fastapi import FastAPI
+
 app = FastAPI()
+
 @app.get("/chat")
 def chat(question: str = "hello"):
 
-    q = question.lower()
+    with open("data/company_policy.txt", "r") as file:
+        policies = file.readlines()
 
-    if "leave" in q:
-        answer = "You can apply leave through the employee portal."
+    answer = "Sorry, I could not find information about that."
 
-    elif "salary" in q:
-        answer = "Salary is credited on the last working day of the month."
-
-    elif "working hours" in q:
-        answer = "Working hours are 9 AM to 6 PM."
-
-    elif "dress code" in q:
-        answer = "Business casual dress code is followed."
-
-    elif "holiday" in q:
-        answer = "Refer to the company holiday calendar."
-
-    else:
-        answer = "Sorry, I do not have information about that."
+    for line in policies:
+        if question.lower() in line.lower():
+            answer = line.strip()
+            break
 
     return {
         "question": question,
